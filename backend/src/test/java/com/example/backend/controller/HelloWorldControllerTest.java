4c87689550037f2be749937712bbeadc712f026d
ryan shade <kyunoske@gmail.com>
2022-10-21 11:39:28 +0200

    Add HelloWorld

---
+++ backend/src/test/java/com/example/backend/controller/HelloWorldControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloWorldControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void sayHello_returnHello() throws Exception {
+        //Given
+
+
+        //When & Then
+        mockMvc.perform(
+                get("/api/hello"))
+                .andExpect(status().is(200))
+                .andExpect(content().string("Hello World!"));
+
+    }
+}
