cc68fc7111edab06b42ed6bf28683e1d20b073be
ryan shade <kyunoske@gmail.com>
2022-10-25 14:31:53 +0200

    Add model, repo, service, controller, with getAll, getById, and add with tests in service and controller

---
+++ backend/src/main/java/com/example/backend/service/TeamService.java
@@ -0,0 +1,40 @@
+package com.example.backend.service;
+
+import com.example.backend.model.Team;
+import com.example.backend.repo.MotoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class TeamService {
+
+    private final MotoRepo repo;
+
+    private final IdService idService;
+
+    @Autowired
+    public TeamService(MotoRepo repo, IdService idService) {
+        this.repo = repo;
+        this.idService = idService;
+    }
+
+    public List<Team> getAllTeams() {
+        return repo.findAll();
+    }
+
+    public Team addTeam(Team team) {
+        team.setId(idService.generateId());
+        return repo.save(team);
+    }
+
+    public Optional<Team> getTeamById(String id) {
+        if (!repo.existsById(id)) {
+            throw new NoSuchElementException("There is no team with id: " + id);
+        }
+        return repo.findById(id);
+    }
+}
---
+++ backend/src/test/java/com/example/backend/controller/TeamControllerTest.java
@@ -0,0 +1,126 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.Team;
+import com.example.backend.repo.MotoRepo;
+import com.example.backend.service.IdService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+class TeamControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private MotoRepo motoRepo;
+
+    @MockBean
+    private IdService idService;
+
+    @Test
+    void getAllTeams() throws Exception {
+        // GIVEN
+        motoRepo.save(new Team("1", "Ducati", "yup", "logo", "1", "2", "3", "4", "5", "10", "1"));
+        motoRepo.save(new Team("2", "Ducati", "yup", "logo", "1", "2", "3", "4", "5", "10", "1"));
+
+        // WHEN & THEN
+        String expectedJSON = """
+                [
+                    {
+                        "id": "1",
+                        "name": "Ducati",
+                        "description": "yup",
+                        "logo": "logo",
+                        "image1":"1",
+                        "image2":"2",
+                        "image3":"3",
+                        "image4":"4",
+                        "image5":"5",
+                        "wins":"10",
+                        "championships": "1"
+                    },
+                    {
+                        "id": "2",
+                        "name": "Ducati",
+                        "description": "yup",
+                        "logo": "logo",
+                        "image1":"1",
+                        "image2":"2",
+                        "image3":"3",
+                        "image4":"4",
+                        "image5":"5",
+                        "wins":"10",
+                        "championships": "1"
+                    }
+                ]
+                """;
+
+        //WHEN+THEN
+        mockMvc.perform(get("/api/teams"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJSON));
+    }
+
+    @Test
+    void addTeam() throws Exception {
+        // GIVEN
+        when(idService.generateId()).thenReturn("1");
+
+        String requestBody = """
+                {
+                            "name": "Ducati",
+                            "description": "yup",
+                            "logo": "logo", 
+                            "image1":"1",
+                            "image2":"2",
+                            "image3":"3",
+                            "image4":"4",
+                            "image5":"5",
+                            "wins":"10",
+                            "championships": "1"
+                    
+                }
+                """;
+
+        String expectedJSON = """
+                {
+                            "id": "1",
+                            "name": "Ducati",
+                            "description": "yup",
+                            "logo": "logo", 
+                            "image1":"1",
+                            "image2":"2",
+                            "image3":"3",
+                            "image4":"4",
+                            "image5":"5",
+                            "wins":"10",
+                            "championships": "1"
+                    
+                }
+                """;
+
+        // WHEN & THEN
+        mockMvc.perform(
+                        post("/api/teams")
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJSON));
+    }
+}
