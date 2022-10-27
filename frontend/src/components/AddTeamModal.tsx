c1ccc510d1054f5951cbf08cb5d396374e5d647f
kyunoske <60664257+kyunoske@users.noreply.github.com>
2022-10-27 12:54:49 +0200

    Update AddTeamModal.tsx

--- frontend/src/components/AddTeamModal.tsx
+++ frontend/src/components/AddTeamModal.tsx
@@ -1,6 +1,5 @@
 import React, {FormEvent, useState} from 'react';
 import {Team} from "../models/Team";
-import {Team} from "../models/Team";
 
 type AddTeamModalProps = {
     addTeam: (team: Team) => void;
