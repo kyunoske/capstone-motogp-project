b173caada2e06a04b30d5db11731ea654fc0589a
ryan shade <kyunoske@gmail.com>
2022-11-09 22:17:16 +0100

    Add filter to articles and back button

--- frontend/src/pages/article/ArticleGallery.tsx
+++ frontend/src/pages/article/ArticleGallery.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import "./ArticleGallery.css";
 import {Article} from "../../models/Article";
 import ArticleCard from "../../components/article/ArticleCard";
+import {Link} from "react-router-dom";
 
 type ArticleGalleryProps = {
     articles: Article[];
 }
 
 function ArticleGallery(props: ArticleGalleryProps) {
-
+    const [data, setData] = useState("");
     const sorting = [...props.articles].sort((a, b) => +a.articleNumber < +b.articleNumber ? 1 : -1)
+    const filteredArticles = sorting.filter((article) => article.title.toLowerCase().includes(data));
 
     return (
         <div>
-            {sorting.map((article, index) =>
-                <ArticleCard article={article} key={index}/>
-            )}
+            <div className="rider-input-container">
+                <Link to={"/"} className="link-to-themes">
+                    <button className="btn button-add back-to-themes">Back to Themes
+                    </button>
+                </Link>
+                <div className="input-group input-group-sm mb-3" style={{width: "50%"}}>
+                    <input style={{width: "80%", borderRadius: "5px"}} type="text" placeholder="Find a rider..."
+                           onChange={(event) => setData(event.target.value)}/>
+                </div>
+            </div>
+
+            <div>
+                {filteredArticles.map((article, index) =>
+                    <ArticleCard article={article} key={index}/>
+                )}
+            </div>
         </div>
     );
 }
